963d2c04beccf212c6badb9bdf456910bce939ac
Akash <tiwariakash95077@gmail.com>
2024-08-29 22:13:16 +0530

    Commit to keep source code safe

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig= {
+    providers: [
+      {
+        domain: "https://powerful-louse-6.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
